9e1ead0ac475d664cf0030ac64df7c1cafdc21a4
Marcus Wyche <marcus.wyche@synergistcomputing.com>
2017-03-06 10:02:07 -0500

    RI-13 Added more tests to RedhawkDriver class

--- redhawk-driver/src/test/java/redhawk/driver/RedhawkDriverTestIT.java
+++ redhawk-driver/src/test/java/redhawk/driver/RedhawkDriverTestIT.java
@@ -25,13 +25,21 @@ import static org.junit.Assert.assertNotNull;
 import java.util.Map;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.omg.CORBA.ORBPackage.InvalidName;
 
+import redhawk.driver.application.RedhawkApplication;
+import redhawk.driver.component.RedhawkComponent;
 import redhawk.driver.domain.RedhawkDomainManager;
+import redhawk.driver.exceptions.ApplicationCreationException;
+import redhawk.driver.exceptions.ApplicationReleaseException;
 import redhawk.driver.exceptions.CORBAException;
+import redhawk.driver.exceptions.MultipleResourceException;
 import redhawk.driver.exceptions.ResourceNotFoundException;
+import redhawk.driver.port.RedhawkPort;
 
 public class RedhawkDriverTestIT {
 	private String domainName;
@@ -42,6 +50,16 @@ public class RedhawkDriverTestIT {
 	
 	private RedhawkDriver driver; 
 	
+	private static RedhawkDriver rhDriver; 
+	
+	private static String sampleApp = "sampleApp";
+	
+	@BeforeClass
+	public static void setupApp() throws ResourceNotFoundException, ApplicationCreationException, CORBAException{
+		rhDriver = new RedhawkDriver("localhost", 2809);
+		rhDriver.getDomain("REDHAWK_DEV").createApplication(sampleApp, "/waveforms/rh/basic_components_demo/basic_components_demo.sad.xml");
+	}
+	
 	@Before
 	public void setup(){
 		domainName = "REDHAWK_DEV";
@@ -50,21 +68,21 @@ public class RedhawkDriverTestIT {
 	}
 	
 	@Test
-	public void testDefaultConstructor() throws CORBAException, ResourceNotFoundException{
+	public void testDefaultConstructor() throws CORBAException, ResourceNotFoundException, MultipleResourceException{
 		driver = new RedhawkDriver();
 		
 		this.basicDriverTests(driver);
 	}
 	
 	@Test
-	public void testOneArgConstructor() throws CORBAException, ResourceNotFoundException{
+	public void testOneArgConstructor() throws CORBAException, ResourceNotFoundException, MultipleResourceException{
 		driver = new RedhawkDriver(hostName);
 		
 		this.basicDriverTests(driver);
 	}
 	
 	@Test
-	public void testTwoArgConstructor() throws CORBAException, ResourceNotFoundException{
+	public void testTwoArgConstructor() throws CORBAException, ResourceNotFoundException, MultipleResourceException{
 		driver = new RedhawkDriver(hostName, domainPort);
 		
 		this.basicDriverTests(driver);
@@ -73,7 +91,7 @@ public class RedhawkDriverTestIT {
 	/*
 	 * Testing of the public RedhawkDriver methods occurs below
 	 */
-	private void basicDriverTests(RedhawkDriver driver) throws CORBAException, ResourceNotFoundException{
+	private void basicDriverTests(RedhawkDriver driver) throws CORBAException, ResourceNotFoundException, MultipleResourceException{
 		//Ensure default port and host and ORB are returned correctly
 		assertEquals(hostName, driver.getHostName());
 		assertEquals(domainPort, driver.getPort());
@@ -91,7 +109,17 @@ public class RedhawkDriverTestIT {
 		assertEquals(domainName, domain.getName());
 		
 		//Get a specific application
+		RedhawkApplication application = driver.getApplication(domainName+"/"+sampleApp);
+		assertNotNull(application);
+		assertEquals(sampleApp, application.getName());
 		
+		//Get a specific component
+		RedhawkComponent component = driver.getComponent(domainName+"/"+sampleApp+"/SigGen_sine.*");
+		assertNotNull(component);
+		
+		//Get a specific port
+		RedhawkPort port = driver.getPort(domainName+"/"+sampleApp+"/SigGen_sine.*/dataFloat_out");
+		assertNotNull(port);		
 	}
 	
 	@After
@@ -99,5 +127,10 @@ public class RedhawkDriverTestIT {
 		driver.disconnect();
 		//TODO: Ask John why getOrb initializes an Orb??? 
 		//assertEquals(null, driver.getOrb());
+	}
+	
+	@AfterClass
+	public static void tearDownApp() throws MultipleResourceException, ResourceNotFoundException, ApplicationReleaseException, CORBAException{
+		rhDriver.getDomain("REDHAWK_DEV").getApplicationByName(sampleApp).release();
 	}
 }

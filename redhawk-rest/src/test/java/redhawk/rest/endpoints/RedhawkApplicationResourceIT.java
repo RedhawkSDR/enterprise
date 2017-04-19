bb09f2d11aaf19be7bb2c05ca3a9c998464b0268
Marcus Wyche <marcus.wyche@synergistcomputing.com>
2017-04-19 15:23:56 -0400

    RI-9 Added headers to necessary files

--- redhawk-rest/src/test/java/redhawk/rest/endpoints/RedhawkApplicationResourceIT.java
+++ redhawk-rest/src/test/java/redhawk/rest/endpoints/RedhawkApplicationResourceIT.java
@@ -1,27 +1,36 @@
+/*
+ * This file is protected by Copyright. Please refer to the COPYRIGHT file
+ * distributed with this source distribution.
+ *
+ * This file is part of REDHAWK __REDHAWK_PROJECT__.
+ *
+ * REDHAWK __REDHAWK_PROJECT__ is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * REDHAWK __REDHAWK_PROJECT__ is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
 package redhawk.rest.endpoints;
 
 import static org.junit.Assert.assertEquals;
 
 import java.util.UUID;
 
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.apache.cxf.jaxrs.client.WebClient;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import redhawk.driver.RedhawkDriver;
-import redhawk.driver.application.RedhawkApplication;
-import redhawk.driver.domain.RedhawkDomainManager;
-import redhawk.driver.exceptions.ApplicationCreationException;
-import redhawk.driver.exceptions.ApplicationReleaseException;
-import redhawk.driver.exceptions.CORBAException;
-import redhawk.driver.exceptions.ResourceNotFoundException;
 import redhawk.rest.model.WaveformInfo;
 
 public class RedhawkApplicationResourceIT extends RedhawkApplicationResourceTestBase{

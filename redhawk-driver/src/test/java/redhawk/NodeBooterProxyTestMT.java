a4ba0a69c172f4cd1168d0fa5ba735d2401508f6
Marcus Wyche <marcus.wyche@synergistcomputing.com>
2017-04-19 14:58:35 -0400

    RI-9 updates to add headers for remain redhawk-driver classes

--- redhawk-driver/src/test/java/redhawk/NodeBooterProxyTestMT.java
+++ redhawk-driver/src/test/java/redhawk/NodeBooterProxyTestMT.java
@@ -1,17 +1,31 @@
-package redhawk.mock;
+/*
+ * This file is protected by Copyright. Please refer to the COPYRIGHT file
+ * distributed with this source distribution.
+ *
+ * This file is part of REDHAWK __REDHAWK_PROJECT__.
+ *
+ * REDHAWK __REDHAWK_PROJECT__ is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * REDHAWK __REDHAWK_PROJECT__ is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+package redhawk;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.junit.Test;
 
-import com.google.common.io.Files;
-
-import redhawk.RedhawkTestBase;
-import redhawk.driver.domain.RedhawkFileManager;
 import redhawk.driver.exceptions.CORBAException;
 import redhawk.driver.exceptions.ConnectionException;
 import redhawk.driver.exceptions.MultipleResourceException;
--- redhawk-driver/src/test/java/redhawk/mock/MyDevice.java
+++
@@ -1,5 +0,0 @@
-package redhawk.mock;
-
-public class MyDevice {
-
-}

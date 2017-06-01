30e9f2f181896baa0c626eb0845f460af74efd5a
Marcus Wyche <marcus.wyche@synergistcomputing.com>
2017-06-01 16:09:40 -0400

    RI-44 Updates to Add EventChannel component

---
+++ demo/redhawk-webapp/src/components/EventChannel.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <div>
+  <!-- Add Event Channel Name and Way to close view -->
+  <md-table v-once>
+    <md-table-header>
+      <md-table-row>
+        <md-table-head>Timestamp</md-table-head>
+        <md-table-head>Message Body</md-table-head>
+      </md-table-row>
+    </md-table-header>
+
+    <md-table-body>
+      <!--<md-table-row v-for="(row, index) in messages"-->
+    </md-table-body>
+  </md-table>
+
+  <md-button class="md-raised">Subscribe</md-button>
+  </div>
+  <div>
+    <h1>Registrants</h1>
+  </div>
+</div>
+</template>

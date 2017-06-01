a07c5ea5193e662f61a789b3aaaf6906aa110c40
Marcus Wyche <marcus.wyche@synergistcomputing.com>
2017-06-01 15:00:15 -0400

    RI-44 updates to add EventChannelManager component

---
+++ demo/redhawk-webapp/src/components/EventChannelManager.vue
@@ -0,0 +1,34 @@
+<template>
+<md-list>
+  <md-list-item>
+    <span>Event Channel [ {{ eventchannelCount }} ]</span>
+    <md-list-expand>
+        <md-list-item
+          class="md-inset"
+          v-for="(eventchannel, index) in eventchannels"
+          v-bind:key="eventchannel"
+          v-bind:index="index">
+          <span>{{ eventchannel.name }}</span>
+          <md-divider></md-divider>
+        </md-list-item>
+    </md-list-expand>
+  </md-list-item>
+</md-list>
+</template>
+
+<script>
+export default{
+  name: 'eventchannels',
+  computed: {
+    eventchannels(){
+      return this.$store.getters.eventchannels
+    },
+    eventchannelCount(){
+      return this.eventchannels.length
+    }
+  }
+}
+</script>
+
+<style>
+</style>

a0bbcaa66d3600ad1a5a3e68c6872e582f1a8ff7
Marcus Wyche <marcus.wyche@synergistcomputing.com>
2017-07-14 15:54:42 -0400

    RELENG-663 updates

--- redhawk-driver/src/test/java/redhawk/testutils/LostEventChannelsIT.java
+++ redhawk-driver/src/test/java/redhawk/testutils/LostEventChannelsIT.java
@@ -2,10 +2,12 @@ package redhawk.testutils;
 
 import org.junit.Test;
 
+import redhawk.driver.eventchannel.RedhawkEventChannel;
 import redhawk.driver.eventchannel.RedhawkEventChannelManager;
 import redhawk.driver.exceptions.CORBAException;
 import redhawk.driver.exceptions.EventChannelCreationException;
 import redhawk.driver.exceptions.MultipleResourceException;
+import redhawk.driver.exceptions.ResourceNotFoundException;
 
 public class LostEventChannelsIT extends RedhawkTestBase{
 	@Test
@@ -17,9 +19,14 @@ public class LostEventChannelsIT extends RedhawkTestBase{
 				System.out.println("Sleeping for ten seconds while event channels hopefully start up");
 				Thread.sleep(10000l);
 				mgr.createEventChannel("HelloWorld");
+				System.out.println("Created event channel "+mgr.getEventChannels());
+				RedhawkEventChannel channel = mgr.getEventChannel("ODM_Channel");
+				System.out.println(channel);
+				channel = mgr.getEventChannel("HelloWorld");
+				System.out.println("Hello "+channel);
 			}
 			System.out.println("OMG found you!!!!!!");
-		} catch (MultipleResourceException | CORBAException | EventChannelCreationException e) {
+		} catch (MultipleResourceException | CORBAException | EventChannelCreationException | ResourceNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}

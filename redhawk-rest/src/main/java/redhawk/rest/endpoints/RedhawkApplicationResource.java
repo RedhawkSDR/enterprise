da070a4a272e2ad8b5e513e24475b52e979abdb3
Marcus Wyche <marcus.wyche@synergistcomputing.com>
2017-07-17 13:48:51 -0400

    RI-63 added endpoint for Application statistics

--- redhawk-rest/src/main/java/redhawk/rest/endpoints/RedhawkApplicationResource.java
+++ redhawk-rest/src/main/java/redhawk/rest/endpoints/RedhawkApplicationResource.java
@@ -48,150 +48,146 @@ import redhawk.rest.model.FetchMode;
 import redhawk.rest.model.FullProperty;
 import redhawk.rest.model.Port;
 import redhawk.rest.model.PortContainer;
+import redhawk.rest.model.PortStatisticsContainer;
 import redhawk.rest.model.Property;
 import redhawk.rest.model.PropertyContainer;
 import redhawk.rest.model.WaveformInfo;
 
 @Path("/{nameserver}/domains/{domain}/applications")
-@Api(value="/{nameserver}/domains/{domain}/applications")
+@Api(value = "/{nameserver}/domains/{domain}/applications")
 public class RedhawkApplicationResource extends RedhawkBaseResource {
 
-    private static Logger logger = Logger.getLogger(RedhawkApplicationResource.class.getName());
+	private static Logger logger = Logger.getLogger(RedhawkApplicationResource.class.getName());
 
-    @ApiParam(value = "url for your name server", required = true)
-    @PathParam("nameserver")
-    private String nameServer;
+	@ApiParam(value = "url for your name server", required = true)
+	@PathParam("nameserver")
+	private String nameServer;
 
-    @ApiParam(value = "Name of REDHAWK Domain", required = true)
-    @PathParam("domain")
-    private String domainName;
+	@ApiParam(value = "Name of REDHAWK Domain", required = true)
+	@PathParam("domain")
+	private String domainName;
 
-    @GET
-    @Path("/")
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    @ApiOperation(
-    		value="GET Applications for a REDHAWK Domain"
-    		)
-    public ApplicationContainer getApplications(@QueryParam("fetch") @DefaultValue("EAGER") FetchMode fetchMode) throws ResourceNotFound, Exception {
-    	return new ApplicationContainer(redhawkManager.getAll(nameServer,
-                        "application", domainName, fetchMode));
-    }
-
-    @GET
-    @Path("/{applicationId}")
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    @ApiOperation(
-    		value="GET Application for a REDHAWK Domain"
-    		)
-    public Application getApplication(@PathParam("applicationId") String applicationId)
-            throws ResourceNotFound, Exception {
-        return redhawkManager.get(nameServer, "application", domainName + "/"
-                        + applicationId);
-    }
-
-    @DELETE
-    @Path("/{applicationId}")
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    @ApiOperation(
-    		value="Release Application from a REDHAWK Domain"
-    		)
-    public Response releaseApplication(@ApiParam(value = "ID for Application") @PathParam("applicationId") String applicationId)
-            throws ResourceNotFound, Exception {
-        redhawkManager
-                .releaseApplication(nameServer, domainName, applicationId);
-        return Response.ok().build();
-    }
-    
-    @POST
-    @Path("/{applicationId}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @ApiOperation(
-    		value="Stop/Start Application"
-    		)
-    public Response controlApplication(@ApiParam(value = "ID for Application") @PathParam("applicationId") String applicationId, String control) throws Exception{
-    	redhawkManager.controlApplication(nameServer, domainName, applicationId, control);
-    	return Response.ok().build();
-    }
-
-    @PUT
-    @Path("/{instanceName}")
-    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    @ApiOperation(
-    		value="Launch Application"
-    		)
-    public Response launchApplication(@ApiParam(value = "Name for Application")
-            @PathParam("instanceName") String instanceName, WaveformInfo info)
-            throws ResourceNotFoundException, ApplicationCreationException {
-        redhawkManager.createApplication(nameServer, domainName, instanceName,
-                info);
-        return Response.ok("Success").build();
-    }
-
-    @GET
-    @Path("/{applicationId}/properties")
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    @ApiOperation(
-    		value="GET Application Properties"
-    		)    
-    public PropertyContainer getApplicationProperties(@ApiParam(value = "ID for Application")
-            @PathParam("applicationId") String applicationId) throws ResourceNotFound,
-            ResourceNotFoundException, Exception {
-        return redhawkManager.getProperties(nameServer, "application",
-                        domainName + "/" + applicationId);
-    }
-
-    @GET
-    @Path("/{applicationId}/properties/{propId}")
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    @ApiOperation(
-    		value="GET Application Property"
-    		)    
-    public Property getApplicationProperty(
-    		@ApiParam(value = "ID/Name for Application") @PathParam("applicationId") String applicationId,
-    		@ApiParam(value = "ID/Name for Property") @PathParam("propId") String propertyId) throws ResourceNotFound,
-            Exception {
-        return redhawkManager.getProperty(propertyId, nameServer,
-                        "application", domainName + "/" + applicationId);
-    }
-
-    @PUT
-    @Path("/{applicationId}/properties/{propId}")
-    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    @ApiOperation(
-    		value="Set Application Property"
-    		)    
-    public Response setApplicationProperty(
-    		@ApiParam(value = "ID for Application") @PathParam("applicationId") String applicationId, 
-    		@ApiParam(value = "ID/Name for Property") @PathParam("propId") String propertyId, FullProperty property)
-            throws Exception {
-        redhawkManager.setProperty(property, nameServer, "application", domainName + "/" + applicationId);
-        return Response.ok().build();
-    }
-    
-    @GET
-    @Path("/{applicationId}/ports")
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    @ApiOperation(
-    		value="GET Application ports"
-    )
-	public ExternalPortContainer getApplicationPorts(@ApiParam(value = "ID/Name for Application") @PathParam("applicationId") String applicationId) throws ResourceNotFoundException, Exception{
-		return new ExternalPortContainer(redhawkManager.getAll(nameServer, "applicationport", domainName+"/"+applicationId, FetchMode.EAGER));
+	@GET
+	@Path("/")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@ApiOperation(value = "GET Applications for a REDHAWK Domain")
+	public ApplicationContainer getApplications(@QueryParam("fetch") @DefaultValue("EAGER") FetchMode fetchMode)
+			throws ResourceNotFound, Exception {
+		return new ApplicationContainer(redhawkManager.getAll(nameServer, "application", domainName, fetchMode));
 	}
-    
-    @GET
-    @Path("/{applicationId}/ports/{portId}")
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    @ApiOperation(
-    		value="GET Application port"
-    )
-    public ExternalPort getApplicationPort(
-    		@ApiParam(value = "ID/Name for Application") @PathParam("applicationId") String applicationId,
-    		@ApiParam(value = "External name for port") @PathParam("portId") String portName) throws ResourceNotFound, Exception {
-        logger.fine("Made it into port call");
-    	ExternalPort port = redhawkManager.get(nameServer, "applicationport", domainName + "/" + applicationId + "/", portName);
-    	logger.fine("Got port: "+port);
-    	return port;
-    }
+
+	@GET
+	@Path("/{applicationId}")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@ApiOperation(value = "GET Application for a REDHAWK Domain")
+	public Application getApplication(@PathParam("applicationId") String applicationId)
+			throws ResourceNotFound, Exception {
+		return redhawkManager.get(nameServer, "application", domainName + "/" + applicationId);
+	}
+
+	@DELETE
+	@Path("/{applicationId}")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@ApiOperation(value = "Release Application from a REDHAWK Domain")
+	public Response releaseApplication(
+			@ApiParam(value = "ID for Application") @PathParam("applicationId") String applicationId)
+			throws ResourceNotFound, Exception {
+		redhawkManager.releaseApplication(nameServer, domainName, applicationId);
+		return Response.ok().build();
+	}
+
+	@POST
+	@Path("/{applicationId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Stop/Start Application")
+	public Response controlApplication(
+			@ApiParam(value = "ID for Application") @PathParam("applicationId") String applicationId, String control)
+			throws Exception {
+		redhawkManager.controlApplication(nameServer, domainName, applicationId, control);
+		return Response.ok().build();
+	}
+
+	@PUT
+	@Path("/{instanceName}")
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@ApiOperation(value = "Launch Application")
+	public Response launchApplication(
+			@ApiParam(value = "Name for Application") @PathParam("instanceName") String instanceName, WaveformInfo info)
+			throws ResourceNotFoundException, ApplicationCreationException {
+		redhawkManager.createApplication(nameServer, domainName, instanceName, info);
+		return Response.ok("Success").build();
+	}
+
+	@GET
+	@Path("/{applicationId}/properties")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@ApiOperation(value = "GET Application Properties")
+	public PropertyContainer getApplicationProperties(
+			@ApiParam(value = "ID for Application") @PathParam("applicationId") String applicationId)
+			throws ResourceNotFound, ResourceNotFoundException, Exception {
+		return redhawkManager.getProperties(nameServer, "application", domainName + "/" + applicationId);
+	}
+
+	@GET
+	@Path("/{applicationId}/properties/{propId}")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@ApiOperation(value = "GET Application Property")
+	public Property getApplicationProperty(
+			@ApiParam(value = "ID/Name for Application") @PathParam("applicationId") String applicationId,
+			@ApiParam(value = "ID/Name for Property") @PathParam("propId") String propertyId)
+			throws ResourceNotFound, Exception {
+		return redhawkManager.getProperty(propertyId, nameServer, "application", domainName + "/" + applicationId);
+	}
+
+	@PUT
+	@Path("/{applicationId}/properties/{propId}")
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@ApiOperation(value = "Set Application Property")
+	public Response setApplicationProperty(
+			@ApiParam(value = "ID for Application") @PathParam("applicationId") String applicationId,
+			@ApiParam(value = "ID/Name for Property") @PathParam("propId") String propertyId, FullProperty property)
+			throws Exception {
+		redhawkManager.setProperty(property, nameServer, "application", domainName + "/" + applicationId);
+		return Response.ok().build();
+	}
+
+	@GET
+	@Path("/{applicationId}/ports")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@ApiOperation(value = "GET Application ports")
+	public ExternalPortContainer getApplicationPorts(
+			@ApiParam(value = "ID/Name for Application") @PathParam("applicationId") String applicationId)
+			throws ResourceNotFoundException, Exception {
+		return new ExternalPortContainer(redhawkManager.getAll(nameServer, "applicationport",
+				domainName + "/" + applicationId, FetchMode.EAGER));
+	}
+
+	@GET
+	@Path("/{applicationId}/ports/{portId}")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@ApiOperation(value = "GET Application port")
+	public ExternalPort getApplicationPort(
+			@ApiParam(value = "ID/Name for Application") @PathParam("applicationId") String applicationId,
+			@ApiParam(value = "External name for port") @PathParam("portId") String portName)
+			throws ResourceNotFound, Exception {
+		logger.fine("Made it into port call");
+		ExternalPort port = redhawkManager.get(nameServer, "applicationport", domainName + "/" + applicationId + "/",
+				portName);
+		logger.fine("Got port: " + port);
+		return port;
+	}
+
+	@GET
+	@Path("/{applicationId}/ports/{portId}/statistics")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@ApiOperation(value = "GET Application port statistics")
+	public PortStatisticsContainer getApplicationPortStatistics(
+			@ApiParam(value = "ID/Name for Application") @PathParam("applicationId") String applicationId,
+			@ApiParam(value = "External name for port") @PathParam("portId") String portName) throws Exception {
+		String applicationPath = domainName+"/"+applicationId+"/"+portName;
+		
+		return redhawkManager.getRhPortStatistics(nameServer, "applicationport", applicationPath);
+	}
 }

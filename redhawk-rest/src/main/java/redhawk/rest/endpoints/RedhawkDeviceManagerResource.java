ee4dfbbb3916913b4d78dad2de1e3df5b8c9005a
Marcus Wyche <marcus.wyche@synergistcomputing.com>
2017-07-25 13:00:22 -0400

    [ci skip] RI-69 preparing for merge w/ develop-2.0

--- redhawk-rest/src/main/java/redhawk/rest/endpoints/RedhawkDeviceManagerResource.java
+++ redhawk-rest/src/main/java/redhawk/rest/endpoints/RedhawkDeviceManagerResource.java
@@ -19,23 +19,30 @@
  */
 package redhawk.rest.endpoints;
 
-import redhawk.driver.exceptions.ResourceNotFoundException;
-import redhawk.rest.exceptions.ResourceNotFound;
-import redhawk.rest.model.DeviceContainer;
-import redhawk.rest.model.DeviceManagerContainer;
-import redhawk.rest.model.FetchMode;
-import redhawk.rest.model.FullProperty;
+import java.util.List;
+import java.util.logging.Logger;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-
-import java.util.List;
-import java.util.logging.Logger;
+import redhawk.driver.exceptions.ResourceNotFoundException;
+import redhawk.rest.exceptions.ResourceNotFound;
+import redhawk.rest.model.DeviceManagerContainer;
+import redhawk.rest.model.FetchMode;
+import redhawk.rest.model.FullProperty;
 
 @Path("/{nameserver}/domains/{domain}/devicemanagers")
 @Api(value="/{nameserver}/domains/{domain}/devicemanagers")

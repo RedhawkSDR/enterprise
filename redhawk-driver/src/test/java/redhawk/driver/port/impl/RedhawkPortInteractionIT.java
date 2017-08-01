652c2dfe756ded83450631ab0dbcb4a8cc5a2262
Marcus Wyche <marcus.wyche@synergistcomputing.com>
2017-08-01 12:41:48 -0400

    Tests for sending and receiving data from dataOctet, dataFloat, dataShort, dataDouble, dataUshort

--- redhawk-driver/src/test/java/redhawk/driver/port/impl/RedhawkPortInteractionIT.java
+++ redhawk-driver/src/test/java/redhawk/driver/port/impl/RedhawkPortInteractionIT.java
@@ -18,6 +18,7 @@ import redhawk.driver.application.RedhawkApplication;
 import redhawk.driver.bulkio.Packet;
 import redhawk.driver.domain.RedhawkFileManager;
 import redhawk.driver.exceptions.ApplicationCreationException;
+import redhawk.driver.exceptions.ApplicationReleaseException;
 import redhawk.driver.exceptions.ApplicationStopException;
 import redhawk.driver.exceptions.CORBAException;
 import redhawk.driver.exceptions.ConnectionException;
@@ -32,61 +33,62 @@ public class RedhawkPortInteractionIT extends RedhawkTestBase{
 	
 	@BeforeClass
 	public static void setup() throws MultipleResourceException, ApplicationCreationException, CORBAException {
+		//Launch App
 		app = driver.getDomain().createApplication("portTest", new File("src/test/resources/waveforms/PortListenerTest/PortListenerTest.sad.xml"));
 	}
 
 	@Test
-	public void connectOctet() throws ApplicationStopException {
-		String portName = "dataOctet_out";
-		try {
-			// Get port
-			RedhawkPort port = app.getComponentByName("DataConverter_1.*").getPort(portName);
+	public void connectOctet() throws ApplicationStopException, ApplicationReleaseException {
+		String[] portNames = {"dataOctet_out", "dataFloat_out", "dataShort_out", "dataDouble_out", "dataUshort_out"};
+		
+		for(String portName : portNames) {
+			try {
+				// Get port
+				RedhawkPort port = app.getComponentByName("DataConverter_1.*").getPort(portName);
 
-			// Send To Port
-			RedhawkPort sendToPort = app.getComponentByName("DataConverter_2.*").getPort("dataOctet");
+				// Send To Port
+				String providesPort = portName.substring(0, portName.indexOf("_"));
+				RedhawkPort sendToPort = app.getComponentByName("DataConverter_2.*").getPort(providesPort);
 
-			// Listen to data on port
-			GenericPortListener pl = new GenericPortListener(sendToPort, true);
+				// Listen to data on port
+				GenericPortListener pl = new GenericPortListener(sendToPort, true);
 
-			port.connect(pl);
-			//Active SRI should be empty because no data has been sent
-			System.out.println(sendToPort.getActiveSRIs());
-			
-			if (!app.isStarted())
-				app.start();
-			while (pl.getMessagesReceived() < 10) {
-				// Loop can't be empty
-				Thread.sleep(1);
+				port.connect(pl);
+				//Active SRI should be empty because no data has been sent
+				assertTrue(sendToPort.getActiveSRIs().isEmpty());
 				
-				//Send data out
-				if(pl.receivedData)
-					sendToPort.send(pl.getPacket());
-			}
+				if (!app.isStarted())
+					app.start();
+				while (pl.getMessagesReceived() < 10) {
+					// Loop can't be empty
+					Thread.sleep(1);
+					
+					//Send data out
+					if(pl.receivedData)
+						sendToPort.send(pl.getPacket());
+				}
 
-			port.disconnect();
-			assertEquals("Should have 10 messages", new Integer(10), pl.getMessagesReceived());
-			
-			//ActiveSRI should be non empty because data has been sent
-			System.out.println(sendToPort.getActiveSRIs());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			fail("Test failure " + e.getMessage());
-		} finally {
-			if (app != null)
-				app.stop();
-		}
-	}
-	
-	@AfterClass
-	public static void cleanupWaveform() throws ConnectionException, ResourceNotFoundException, CORBAException {
-		try {
-			RedhawkFileManager manager = driver.getDomain(domainName).getFileManager();
-			manager.removeDirectory("/waveforms/PortListenerTest");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.err.println("Clean this up eventaully");
+				port.disconnect();
+				assertEquals("Should have 10 messages", new Integer(10), pl.getMessagesReceived());
+				
+				//ActiveSRI should be non empty because data has been sent
+				assertTrue(!sendToPort.getActiveSRIs().isEmpty());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				fail("Test failure " + e.getMessage());
+			} finally {
+				//Release app
+				if (app != null)
+					app.release();
+				
+				//Relaunch app for next test 
+				try {
+					app = driver.getDomain().createApplication("testPorts", "/waveforms/PortListenerTest/PortListenerTest.sad.xml");
+				} catch (MultipleResourceException | ApplicationCreationException | CORBAException e) {
+					fail("Broken test logic should be able to re deploy from that location.");
+				}
+			}
 		}
 	}
 
@@ -216,6 +218,18 @@ public class RedhawkPortInteractionIT extends RedhawkTestBase{
 		
 		public Packet getPacket() {
 			return this.packet;
+		}
+	}	
+	
+	@AfterClass
+	public static void cleanupWaveform() throws ConnectionException, ResourceNotFoundException, CORBAException {
+		try {
+			RedhawkFileManager manager = driver.getDomain(domainName).getFileManager();
+			manager.removeDirectory("/waveforms/PortListenerTest");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.err.println("Clean this up eventaully");
 		}
 	}
 }

1fed2151e6c562f1e3ea988c88b1e70ef0e10108
Marcus Wyche <marcus.wyche@synergistcomputing.com>
2017-08-23 12:08:08 -0400

    [ci skip] RI-109 updates to add vuex

--- demo/redhawk-metrics/src/RedhawkMetrics.vue
+++ demo/redhawk-metrics/src/RedhawkMetrics.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
     <h1>Hello World</h1>
+    <button v-on:click="available">Available</button>
     <tree-view :data="sampleData" max-depth="7"></tree-view>
   </div>
 </template>
 
 <script>
-import TreeView from "vue-json-tree-view"
-
 export default {
   name: 'redhawkmetrics',
   data(){
     return {
       sampleData: {"glossary":{"title":"example glossary","GlossDiv":{"title":"S","GlossList":{"GlossEntry":{"ID":"SGML","SortAs":"SGML","GlossTerm":"Standard Generalized Markup Language","Acronym":"SGML","Abbrev":"ISO 8879:1986","GlossDef":{"para":"A meta-markup language, used to create markup languages such as DocBook.","GlossSeeAlso":["GML","XML"]},"GlossSee":"markup"}}}}}
+    }
+  },
+  methods: {
+    available: function(){
+      console.log("Making it to available")
+      console.log(this.$store)
+      this.$store.dispatch("getAvailableMetrics")
     }
   }
 }
--- demo/redhawk-metrics/src/main.js
+++ demo/redhawk-metrics/src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 import TreeView from "vue-json-tree-view"
 
 Vue.config.productionTip = false
@@ -13,6 +14,7 @@ Vue.use(TreeView)
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })

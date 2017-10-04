27ab4c72953c127ed027ad7fdfa3597627506387
Marcus Wyche <marcus.wyche@synergistcomputing.com>
2017-10-04 13:07:24 -0400

    RI-131 updates to make sure wars are deployed to nexus. RI-132 updates to clean up dists project structure to support karaf & jetty. This is also now more flexible for eventually supporting other types of distributions

--- dist/src/main/scripts/preinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Create redhawk system account if it doesn't already exist
-groupadd -r -f redhawk
-if ! id redhawk &> /dev/null; then
-  # -M is don't create home dir, -r is system account, -s is shell
-  # -c is comment, -n is don't create group, -g is group name/id
-  /usr/sbin/useradd -M -r -s /sbin/nologin \
-    -c "REDHAWK System Account" -n -g redhawk redhawk > /dev/null
-fi

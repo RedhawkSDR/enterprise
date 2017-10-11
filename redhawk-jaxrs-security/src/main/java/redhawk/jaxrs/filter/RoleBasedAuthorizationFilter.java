7a32c5bae575e40c1d1b8457a87eb03d6561c655
Marcus Wyche <marcus.wyche@synergistcomputing.com>
2017-10-11 12:54:03 -0400

    RI-132 added additional logging configuration and cleaned up logging.additions

--- redhawk-jaxrs-security/src/main/java/redhawk/jaxrs/filter/RoleBasedAuthorizationFilter.java
+++ redhawk-jaxrs-security/src/main/java/redhawk/jaxrs/filter/RoleBasedAuthorizationFilter.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Collectors;
 

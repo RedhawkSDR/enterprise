3bf5fcd1851ec678e0a4aeb00d0ae62dd4814381
Marcus Wyche <marcus.wyche@synergistcomputing.com>
2017-10-13 12:50:34 -0400

    RI-120 added CORS filter

--- redhawk-jaxrs-security/src/main/java/redhawk/jaxrs/filter/CORSFilter.java
+++ redhawk-jaxrs-security/src/main/java/redhawk/jaxrs/filter/CORSFilter.java
@@ -2,8 +2,6 @@ package redhawk.jaxrs.filter;
 
 import java.io.IOException;
 
-import javax.annotation.Priority;
-import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;

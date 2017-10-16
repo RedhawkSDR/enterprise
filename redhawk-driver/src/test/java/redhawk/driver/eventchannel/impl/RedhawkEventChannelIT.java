9d03cc2e9ea0611b35c1673a575ba0811f6962c7
Marcus Wyche <marcus.wyche@synergistcomputing.com>
2017-10-16 14:50:39 -0400

    DOCS-716 cleaning up docs

--- redhawk-driver/src/test/java/redhawk/driver/eventchannel/impl/RedhawkEventChannelIT.java
+++ redhawk-driver/src/test/java/redhawk/driver/eventchannel/impl/RedhawkEventChannelIT.java
@@ -43,7 +43,7 @@ import redhawk.driver.exceptions.MultipleResourceException;
 import redhawk.driver.exceptions.ResourceNotFoundException;
 import redhawk.testutils.RedhawkTestBase;
 
-public class RedhawkEventChannelMT extends RedhawkTestBase{
+public class RedhawkEventChannelIT extends RedhawkTestBase{
 	@Test
 	public void testGetRegistrants() throws MultipleResourceException, ResourceNotFoundException, CORBAException{
 		RedhawkEventChannelImpl impl = (RedhawkEventChannelImpl) driver.getDomain().getEventChannelManager().getEventChannel("IDM_Channel");

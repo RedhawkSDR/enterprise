ff50bfccebd7f4094bc7b9b66f5f776f4786fae0
Marcus Wyche <marcus.wyche@synergistcomputing.com>
2017-10-23 12:27:38 -0400

    RI-112 updates for DeviceManager view

---
+++ demos/redhawk-webapp/src/components/Latest/devicemanagers/DeviceManager.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="content">
+  <div class="container-fluid">
+    <div class="row">
+
+    </div>
+    <div class="row">
+      <properties
+        :bgColor="green"
+        :id="devicemanager.label"
+        :properties="devicemanager.properties" />
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import Properties from '../properties/Properties.vue'
+
+export default {
+  name: 'devicemanager',
+  mounted(){
+    this.$store.dispatch('selectDeviceManager', this.$route.params.devicemanagerName)
+  },
+  computed: {
+    devicemanager(){
+      return this.$store.getters.devicemanager
+    }
+  },
+  components: {
+    'properties' : Properties
+  }
+}
+</script>
